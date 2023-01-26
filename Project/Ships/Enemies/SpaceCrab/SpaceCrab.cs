80d3408e50a5bbd71b121240b3f3f6c5a4382251
Jake <jwinkler1493@gmail.com>
2023-01-25 21:27:07 -0500

    Applied movement values to spacecrab agents

--- Project/Ships/Enemies/SpaceCrab/SpaceCrab.cs
+++ Project/Ships/Enemies/SpaceCrab/SpaceCrab.cs
@@ -5,11 +5,33 @@ using System;
 using System.Collections.Generic;
 using StarSwarm.Project.SWStateMachine;
 using StarSwarm.Project.GSAI_Framework.Agents;
+using StarSwarm.Project.Utils;
 
 namespace StarSwarm.Project.Ships.Enemies.SpaceCrab
 {
     public class SpaceCrab : KinematicBody2D
     {
+        [Export]
+        public float HealthMax = 100f;
+        [Export]
+        public float LinearSpeedMax = 400f;
+        [Export]
+        public float AccelerationMax = 600f;
+        [Export]
+        public float DragFactor = 0.04f;
+        [Export]
+        public float AngularSpeedMax = 200;
+        [Export]
+        public float AngularAccelerationMax = 3600f;
+        [Export]
+        public float AngularDragFactor = 0.05f;
+        [Export]
+        public float DistanceFromTargetMin = 200f;
+        [Export]
+        public float DistanceFromObstaclesMin = 200f;
+        [Export(PropertyHint.Layers2dPhysics)]
+        public Int64 ProjectileMask = 0;
+
         public StateMachine StateMachine = null!;
         public Events Events = new Events();
 
@@ -25,6 +47,15 @@ namespace StarSwarm.Project.Ships.Enemies.SpaceCrab
         public override void _Ready()
         {
             StateMachine = GetNode<StateMachine>("StateMachine");
+
+            Agent.LinearAccelerationMax = AccelerationMax;
+            Agent.LinearSpeedMax = LinearSpeedMax;
+
+            Agent.AngularAccelerationMax = Mathf.Deg2Rad(AngularAccelerationMax);
+            Agent.AngularSpeedMax = Mathf.Deg2Rad(AngularSpeedMax);
+
+            Agent.LinearDragPercentage = DragFactor;
+            Agent.AngularDragPercentage = AngularDragFactor;
 
             Events = GetNode<Events>("/root/Events");
             Events.Connect("TargetAggroed", this, "OnTargetAggroed");
