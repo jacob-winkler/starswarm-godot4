5055bf751b1d8572b03dfd844dc65add689d7eff
jacob-winkler <jakegk14@yahoo.com>
2023-12-12 21:40:14 -0500

    Add sentient goo spawner

--- StarSwarm/World/GameWorld.cs
+++ StarSwarm/World/GameWorld.cs
@@ -7,45 +7,45 @@ using StarSwarm.World.Spawners;
 namespace StarSwarm.World;
 
 public partial class GameWorld : Node2D
-	{
-		[Export]
-		public float Radius = 8000.0f;
+{
+	[Export]
+	public float Radius = 8000.0f;
 
-		public RandomNumberGenerator Rng { get; set; } = new RandomNumberGenerator();
-		public PlayerSpawner PlayerSpawner { get; set; } = default!;
-		public SpaceCrabSpawner SpaceCrabSpawner { get; set; } = default!;
-		public PlanetSpawner PlanetSpawner { get; set; } = default!;
-		public PlayerShip Player { get; set; } = default!;
-		public HealthBarUpdater HealthBarUpdater { get; set; } = default!;
+	public RandomNumberGenerator Rng { get; set; } = new RandomNumberGenerator();
+	public PlayerSpawner PlayerSpawner { get; set; } = default!;
+	public SpaceCrabSpawner SpaceCrabSpawner { get; set; } = default!;
+	public PlanetSpawner PlanetSpawner { get; set; } = default!;
+	public PlayerShip Player { get; set; } = default!;
+	public HealthBarUpdater HealthBarUpdater { get; set; } = default!;
     public GameOver GameOverScreen { get; set; } = default!;
 
     private bool _playerDead;
 
-		public override async void _Ready()
-		{
-			await ToSignal(Owner, "ready");
-			base._Ready();
+	public override async void _Ready()
+	{
+		await ToSignal(Owner, "ready");
+		base._Ready();
 
-			Rng.Randomize();
+		Rng.Randomize();
 
-			PlayerSpawner = GetNode<PlayerSpawner>("PlayerSpawner");
-			Player = GetNode<PlayerShip>("PlayerSpawner/PlayerShip");
-			SpaceCrabSpawner = GetNode<SpaceCrabSpawner>("SpaceCrabSpawner");
+		PlayerSpawner = GetNode<PlayerSpawner>("PlayerSpawner");
+		Player = GetNode<PlayerShip>("PlayerSpawner/PlayerShip");
+		SpaceCrabSpawner = GetNode<SpaceCrabSpawner>("SpaceCrabSpawner");
         PlanetSpawner = GetNode<PlanetSpawner>("PlanetSpawner");
         HealthBarUpdater = GetNode<HealthBarUpdater>("HealthBarUpdater");
         GameOverScreen = GetNode<GameOver>("UI/GameOver");
 
         Setup();
-		}
+	}
 
-		public void Setup()
-		{
-			Player.Connect("Died", new Callable(this, "OnPlayerDied"));
+	public void Setup()
+	{
+		Player.Connect("Died", new Callable(this, "OnPlayerDied"));
 
-			PlayerSpawner.SpawnPlayer();
+		PlayerSpawner.SpawnPlayer();
 
-			SpaceCrabSpawner.Initialize(Player, Rng);
-			SpaceCrabSpawner.SpawnSpaceCrabsAroundPlayer();
+		SpaceCrabSpawner.Initialize(Player, Rng);
+		SpaceCrabSpawner.SpawnSpaceCrabsAroundPlayer();
 
         PlanetSpawner.Initialize(Player, Rng);
         PlanetSpawner.SpawnInitialPlanets();
@@ -54,26 +54,26 @@ public partial class GameWorld : Node2D
         GameOverScreen.ProcessMode = ProcessModeEnum.Always;
     }
 
-		public override void _PhysicsProcess(double delta)
-		{
-			if(!_playerDead)
-				UpdateHealthBarPosition();
-		}
+	public override void _PhysicsProcess(double delta)
+	{
+		if(!_playerDead)
+			UpdateHealthBarPosition();
+	}
 
-		private void UpdateHealthBarPosition()
-		{
-			var healthBarPosition = Player.GlobalPosition;
-			healthBarPosition.X -= 16;
-			healthBarPosition.Y += 24;
-			HealthBarUpdater.SetGlobalPosition(healthBarPosition);
-		}
+	private void UpdateHealthBarPosition()
+	{
+		var healthBarPosition = Player.GlobalPosition;
+		healthBarPosition.X -= 16;
+		healthBarPosition.Y += 24;
+		HealthBarUpdater.SetGlobalPosition(healthBarPosition);
+	}
 
-		private void OnPlayerDied()
-		{
-			_playerDead = true;
+	private void OnPlayerDied()
+	{
+		_playerDead = true;
         GameOverScreen.GlobalPosition = Player.GlobalPosition;
         GameOverScreen.Start();
 
         GetTree().Paused = true;
-		}
 	}
+}
