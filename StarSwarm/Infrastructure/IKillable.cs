f9fc2a1734cb50ff24b8f50d9158e73cb8c67705
jacob-winkler <jakegk14@yahoo.com>
2024-01-31 17:50:49 -0500

    Begin refactoring damage
    The refactor should support different types of damage being delivered,
    and different types of armor/shields handling that damage in their own
    ways.

---
+++ StarSwarm/Infrastructure/IKillable.cs
@@ -0,0 +1,10 @@
+﻿namespace StarSwarm.StarSwarm.Infrastructure;
+public interface IKillable
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="damage"></param>
+    /// <param name="origin"></param>
+    void TakeDamage(float damage, object origin); // origin -- consider adding a custom object to represent attributes about the damage.
+}
